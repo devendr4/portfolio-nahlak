191794197bc352c2c2147e237175669871821d0e
devendr4 <angel.emo.altuve@gmail.com>
2020-12-13 23:24:21 -0400

    skill grid

--- src/components/SkillsComponent.tsx
+++ src/components/SkillsComponent.tsx
@@ -3,33 +3,48 @@ import { skillData } from "../data";
 
 import "../component-styles/skills.scss";
 
-export const SkillList = (category: string) => {
+const SkillList = ({ category }: { category: string }) => {
   let skills = skillData.map((skill, i) => {
-    return (
-      <div key={i}>
-        <skill.icon />
-        <p>{skill.name}</p>
-      </div>
-    );
+    if (category === skill.category) {
+      return (
+        <span key={i}>
+          <skill.icon />
+          <p>{skill.name}</p>
+          <div>
+            <div>
+              <div></div>
+            </div>
+          </div>
+        </span>
+      );
+    }
   });
 
-  return <>{skills}</>;
+  return <div className="skill-group">{skills}</div>;
 };
 
 export const Skills = () => {
   return (
     <div className="skills">
-      <h3>Frontend Frameworks</h3>
-      <SkillList {..."s"} />
-
       <div>
+        <h3>Languages</h3>
+        <SkillList category="lang" />
+      </div>
+      <div>
+        <h3>Frontend Frameworks</h3>
+        <SkillList category="frontend" />
+      </div>
+      <div className="skill-group">
         <h3>Databases</h3>
+        <SkillList category="database" />
       </div>
       <div>
         <h3>Backend Frameworks</h3>
+        <SkillList category="backend" />
       </div>
       <div>
         <h3>Business Intelligence</h3>
+        <SkillList category="bi" />
       </div>
     </div>
   );
